aa4021ec80d775d3a313349ae7c9c45f5211bc4d
unknown <pranavrshah2012@gmail.com>
2014-03-08 23:51:44 -0800

    added camera permissions in the manifest.

--- src/com/example/camerawithbuttons/MainActivity.java
+++ src/com/example/camerawithbuttons/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends Activity implements LocationListener {
         //end of location code
 
 	}
-
+	
 	protected void onResume(){
 		super.onResume();
 		Log.v("Function", "in onResume()");
