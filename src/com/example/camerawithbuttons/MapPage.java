2a38ea4c1ea0a72602fd9f59fa0a289626ea32b2
unknown <pranavrshah2012@gmail.com>
2014-04-19 01:43:38 -0500

    class file changes!

--- src/com/example/camerawithbuttons/MapPage.java
+++ src/com/example/camerawithbuttons/MapPage.java
@@ -44,7 +44,7 @@ private LocationManager locationManager;
 private String provider;
 Criteria criteria;
 Location location;
-LatLng coordinates;
+LatLng coordinates;z
 CameraPosition cameraPosition;
 
 @Override
